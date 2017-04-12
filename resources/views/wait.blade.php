8d2ed115f084f13c8fd4018746248564a452a2e4
ROBERTO <lambis.net@gmail.com>
2017-04-12 17:00:19 +0200

    add server ping

--- resources/views/wait.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.auth')
-@section('page_title')
-    @lang('pages.login')
-@endsection
-@section('content')
-    <h1>Welcome</h1>
-    <p>Looks like your account is not active. Please wait while the activation process are completed or contact your referent.</p>
-@endsection
